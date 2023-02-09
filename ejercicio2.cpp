f9e9d335346bb4b33a32866397c275c31d8cceb1
CerberusX99 <114266496+CerberusX99@users.noreply.github.com>
2023-02-09 15:22:24 -0600

    Add files via upload

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,10 +1,7 @@
 #include<iostream>
 using namespace std;
-int main(){
-	int anoac, mesac, mes, ano, dia, diac, anos;
-	anoac=2022;
-	mesac=2;
-	diac=9;
+int anoac, mesac, mes, ano, dia, diac, anos;
+void PedirFecha(){
 	cout<<"Calculadora de edad"<<endl;
 	cout<<"Ingresa ano"<<endl;
 	cin>>ano;
@@ -13,10 +10,22 @@ int main(){
 	cout<<"Ingresa dia"<<endl;
 	cin>>dia;
 	cout<<"Listo"<<endl;
+}
+void Calcular(){
 	anos=anoac-ano;
 	if(mesac>=mes&&diac>dia){
 		anos=anos+1;
 	}
+}
+void Mostrar(){
 	cout<<"Tu edad es: "<<anos<<" anos"<<endl;
+}
+int main(){
+	anoac=2022;
+	mesac=2;
+	diac=9;
+	PedirFecha();
+	Calcular();
+	Mostrar();
 	return 0;
 }
