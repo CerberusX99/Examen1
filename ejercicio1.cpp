7adb3b16f20be1069094334f3cf37d4f603235e9
CerberusX99 <114266496+CerberusX99@users.noreply.github.com>
2023-02-09 15:21:35 -0600

    Add files via upload

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,13 +1,15 @@
 #include<iostream>
 using namespace std;
-int main(){
-	int num1, num2, num3;
+int num1, num2, num3;
+void PedirNum(){
 	cout<<"Ingresa un numero"<<endl;
 	cin>>num1;
 	cout<<"Ingresa un numero"<<endl;
 	cin>>num2;
 	cout<<"Ingresa un numero"<<endl;
 	cin>>num3;
+}
+void NumMay(){
 	if(num1>num2&&num1>num3){
 		cout<<"El numero mayor es: "<<num1<<endl;
 	}
@@ -17,6 +19,8 @@ int main(){
 	else{
 		cout<<"El numero mayor es: "<<num3<<endl;
 	}
+}
+void NumMen(){
 	if(num1<num2&&num1<num3){
 		cout<<"El numero menor es: "<<num1<<endl;
 	}
@@ -26,4 +30,10 @@ int main(){
 	else{
 		cout<<"El numero menor es: "<<num3<<endl;
 	}
+}
+int main(){
+	PedirNum();
+	NumMay();
+	NumMen();
+	return 0;
 }
