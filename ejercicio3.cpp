3537fab11c9c543566b1d5c32302592fff545b6c
CerberusX99 <114266496+CerberusX99@users.noreply.github.com>
2023-02-09 15:22:45 -0600

    Add files via upload

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,12 +1,18 @@
 #include<iostream>
 using namespace std;
-int main(){
-	int num;
+int num;
+void PedirN(){
 	cout<<"Ingresa el numero que quieres para la tabla"<<endl;
 	cin>>num;
+}
+void Mostrartabla(){
 	for(int i=1; i<=10; i++){
 		int mul=i*num;
 		cout<<num<<"*"<<i<<"="<<mul<<endl;
 	}
+}
+int main(){
+	PedirN();
+	Mostrartabla();
 	return 0;
 }
